7d3c1c54c7b41c7ff80693c3c3e9cdc6e5e1f3d6
zandalman <zandalman@gmail.com>
2024-05-02 21:58:04 -0400

    added physical unit sim

--- const.py
+++ const.py
@@ -1,2 +1,9 @@
-c   = 1     # speed of light
-gam = 5/3 # adiabatic index
+c   = 2.99792458e10 # speed of light
+gam = 5/3           # adiabatic index
+Msol = 1.99e33      # solar mass
+
+pc  = 3.086e18      # parsec
+hr  = 3600          # hour
+day = 24*hr         # day
+yr  = 365*day       # year
+kyr = 1000*yr
