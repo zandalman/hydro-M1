338f0cadfe7797b05a54c0b7f1de8da975f6e628
zandalman <zandalman@gmail.com>
2024-05-02 22:30:53 -0400

    units

--- const.py
+++ const.py
@@ -1,2 +1,11 @@
-c   = 1     # speed of light
-gam = 5/3 # adiabatic index
+c   = 2.99792458e10 # speed of light
+gam = 5/3           # adiabatic index
+Msol = 1.99e33      # solar mass
+mH   = 1.6733e-24   # Hydrogen mass
+kB   = 5.67051e-5   # Boltzmann constant
+
+pc  = 3.086e18      # parsec
+hr  = 3600          # hour
+day = 24*hr         # day
+yr  = 365*day       # year
+kyr = 1000*yr
