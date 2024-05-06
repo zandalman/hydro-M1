760e83be911a7785c80612e568b95a1ca3f360c9
yuepanexplorer <140010825+yuepanexplorer@users.noreply.github.com>
2024-05-06 11:14:27 -0400

    Update const.py

--- const.py
+++ const.py
@@ -1,9 +1,31 @@
 c   = 2.99792458e10 # speed of light
 gam = 5/3           # adiabatic index
 Msol = 1.99e33      # solar mass
+mH   = 1.6733e-24   # Hydrogen mass
+kB   = 5.67051e-5   # Boltzmann constant
 
 pc  = 3.086e18      # parsec
 hr  = 3600          # hour
 day = 24*hr         # day
 yr  = 365*day       # year
 kyr = 1000*yr
+
+sigma_HI_N = 6.346296358990505e-18 # average cross section of HI
+sigma_HI_E = 6.346296358990505e-18 # energy-weighted cross section of HI
+e_photon   = 20.0*1.602e-12        # energy of photons i nunits of erg, 1eV =  1.602.10-12  erg
+e_HI       = 13.6*1.602e-12        # photo ionization energy, which is 13.6 eV
+mH         = 1.6733*1e-24          # Hydrogen mass in cgs units
+kB         = 1.380658*1e-16        # Boltzmann constant in cgs units
+cr         = c*6e-4                # reduced speed of light in units of cm/s (the real speed of light is 3e10 cm/s, fc_min=6e-4)
+X          = 1                     # the hydrogen mass fraction
+mu         = 1                     # average mass per particle in the has in units of m_H
+aexp       = 1                     # cosmological expansion factor
+
+# photoionization cross section parameters for HI (Table E1)
+ε0     = 0.4298
+y0     = 0
+ya     = 32.88
+yw     = 0
+y1     = 0
+P      = 2.963
+sigma0 = 5.475 * 1e-14
